88a308e9a09d490ef5c15f3779308866cb454e4e
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-01-07 15:02:43 +0100

    Add main component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Main from './components/Main';
 import background from './assets/images/backgrounds/beach.jpg'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <img className="background" alt="background" src={background}/>
       <Navbar />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ExpandDown from './Icons/ExpandDown.svg';
+import Plane from './Icons/Plane.svg';
+import ArrowRight from './Icons/ArrowRight.svg';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main__pre-header">
+        <span className="main__pre-header -active">Fly With Us To</span>
+      </div>
+
+      <h2 className="main__header">Bangkok</h2>
+
+      <p className="main__description">A new door to paradise for just 15000</p>
+
+      <div className="main__details">
+        <p>Book now till 25 Oct 2019</p>
+        <p>Travel from 25 Oct 2019 to 25 Dec 2019</p>
+      </div>
+
+      <button className="main__button">
+        <span className="main__button-content">Let's Go</span>
+        <img src={ArrowRight} alt="" className="main__button-icon"/>
+      </button>
+
+      <div className="main__search">
+        <div className="main__search-icon">
+          <img src={Plane} alt="airplane"/>
+        </div>
+
+        <div className="main__search-item">
+          <p>Departure airport</p>
+          <p className="selection">Mumbai (BOM)
+            <img src={ExpandDown} alt="" />
+          </p>
+        </div>
+
+        <div className="main__search-item">
+          <p>Arrival airport</p>
+          <p className="selection">Aalborg (AAL)
+            <img src={ExpandDown} alt="" />
+          </p>
+        </div>
+
+        <button className="main__search-button">
+          Search
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Main
+
