64619d120ac66fccdbb6f6b7d87e31c0fc70d5f6
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-01-24 11:42:24 +0100

    Add desktop designs for experience cards

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,15 +3,28 @@ import Play from './Icons/Play.svg';
 
 const Experience = () => {
   const cards = [
-    { muted: 'Arrive Inspired', text: 'Business Class', image: 'https://www.placecage.com/c/600/250' },
-    { muted: 'Expect the exceptional', text: 'Economy Class', image: 'https://www.placecage.com/c/600/250' },
-    { muted: 'Award winning technology', text: 'Explore the fleed in AR', image: 'https://www.placecage.com/c/600/250' },
+    {
+      muted: 'Arrive Inspired',
+      text: 'Business Class',
+      image: 'https://www.placecage.com/c/800/800'
+    },
+    {
+      muted: 'Expect the exceptional',
+      text: 'Economy Class',
+      image: 'https://www.placecage.com/c/800/800'
+    },
+    {
+      muted: 'Award winning technology',
+      text: 'Explore the fleed in AR',
+      image: 'https://www.placecage.com/c/800/800',
+      expanded: 'Explore the Zabdos A380 or Boeing 777 using our 3D experience',
+    },
   ]
 
   return (
     <div className="experience">
-      <div className="experience__separator" />
       <div className="experience__header">
+        <div className="experience__separator" />
         <h2 className="experience__header-main">The Zadbos Experience</h2>
         <p className="experience__header-muted">Whether it's your first flight or simply your latest, we work to anticipate your every need</p>
         <div className="experience__header-video">
@@ -20,9 +33,15 @@ const Experience = () => {
         </div>
       </div>
 
-      {cards.map(({ muted, text, image }) => (
+      {cards.map(({ muted, text, image, expanded }) => (
         <div className="experience__card">
-          <img className="experience__card-image" src={image} alt="" />
+          <div className="experience__card-image">
+            <img src={image} alt="" />
+            <div className="experience__card-expanded">
+              <img src={Play} alt="" />
+              <span>{expanded}</span>
+            </div>
+          </div>
           <div className="experience__card-container">
             <p className="experience__card-content">{muted}</p>
             <div className="experience__card-separator"/>
